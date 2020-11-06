41e47da1c3b68cc9322fe4246816908470565696
CaptainProninInSpace <sergey_rozhkov1@mail.ru>
2020-11-06 15:38:33 +0300

    1

--- EmailGenerator/Form1.Designer.cs
+++ EmailGenerator/Form1.Designer.cs
@@ -28,32 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.MainConsole = new System.Windows.Forms.RichTextBox();
-            this.default_Secondleveldomain = new System.Windows.Forms.TextBox();
+            this.Default_SecondLevelDomain = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.default_FirstLeveldomain = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.Checkbox_defaultdomain = new System.Windows.Forms.CheckBox();
-            this.Numberof_emails = new System.Windows.Forms.TextBox();
+            this.EmailsNumText = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.btn_GenerateEmails = new System.Windows.Forms.Button();
+            this.Password_Length = new System.Windows.Forms.TextBox();
+            this.password_label = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // MainConsole
             // 
             this.MainConsole.Location = new System.Drawing.Point(60, 34);
             this.MainConsole.Name = "MainConsole";
+            this.MainConsole.ReadOnly = true;
             this.MainConsole.Size = new System.Drawing.Size(268, 445);
             this.MainConsole.TabIndex = 0;
             this.MainConsole.Text = "";
             // 
-            // default_Secondleveldomain
+            // Default_SecondLevelDomain
             // 
-            this.default_Secondleveldomain.Location = new System.Drawing.Point(362, 114);
-            this.default_Secondleveldomain.Name = "default_Secondleveldomain";
-            this.default_Secondleveldomain.Size = new System.Drawing.Size(131, 22);
-            this.default_Secondleveldomain.TabIndex = 1;
+            this.Default_SecondLevelDomain.Location = new System.Drawing.Point(362, 114);
+            this.Default_SecondLevelDomain.Name = "Default_SecondLevelDomain";
+            this.Default_SecondLevelDomain.Size = new System.Drawing.Size(131, 22);
+            this.Default_SecondLevelDomain.TabIndex = 1;
             // 
             // label1
             // 
@@ -100,12 +104,13 @@
             this.Checkbox_defaultdomain.Text = "Задать определенный домен";
             this.Checkbox_defaultdomain.UseVisualStyleBackColor = true;
             // 
-            // Numberof_emails
+            // EmailsNumText
             // 
-            this.Numberof_emails.Location = new System.Drawing.Point(539, 173);
-            this.Numberof_emails.Name = "Numberof_emails";
-            this.Numberof_emails.Size = new System.Drawing.Size(100, 22);
-            this.Numberof_emails.TabIndex = 7;
+            this.EmailsNumText.Location = new System.Drawing.Point(539, 173);
+            this.EmailsNumText.Name = "EmailsNumText";
+            this.EmailsNumText.Size = new System.Drawing.Size(100, 22);
+            this.EmailsNumText.TabIndex = 7;
+            this.EmailsNumText.Text = "1";
             // 
             // label4
             // 
@@ -124,22 +129,42 @@
             this.btn_GenerateEmails.TabIndex = 9;
             this.btn_GenerateEmails.Text = "Сгенерировать адреса";
             this.btn_GenerateEmails.UseVisualStyleBackColor = true;
+            this.btn_GenerateEmails.Click += new System.EventHandler(this.btn_GenerateEmails_Click);
+            // 
+            // Password_Length
+            // 
+            this.Password_Length.Location = new System.Drawing.Point(539, 229);
+            this.Password_Length.Name = "Password_Length";
+            this.Password_Length.Size = new System.Drawing.Size(100, 22);
+            this.Password_Length.TabIndex = 10;
+            this.Password_Length.Text = "8";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(359, 232);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(113, 17);
+            this.password_label.TabIndex = 11;
+            this.password_label.Text = "Размер пароля:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(726, 588);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.Password_Length);
             this.Controls.Add(this.btn_GenerateEmails);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.Numberof_emails);
+            this.Controls.Add(this.EmailsNumText);
             this.Controls.Add(this.Checkbox_defaultdomain);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.default_FirstLeveldomain);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.default_Secondleveldomain);
+            this.Controls.Add(this.Default_SecondLevelDomain);
             this.Controls.Add(this.MainConsole);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Email Generator";
             this.ResumeLayout(false);
@@ -150,15 +175,17 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox MainConsole;
-        private System.Windows.Forms.TextBox default_Secondleveldomain;
+        private System.Windows.Forms.TextBox Default_SecondLevelDomain;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox default_FirstLeveldomain;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.CheckBox Checkbox_defaultdomain;
-        private System.Windows.Forms.TextBox Numberof_emails;
+        private System.Windows.Forms.TextBox EmailsNumText;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btn_GenerateEmails;
+        public System.Windows.Forms.CheckBox Checkbox_defaultdomain;
+        private System.Windows.Forms.Label password_label;
+        public System.Windows.Forms.TextBox Password_Length;
     }
 }
 
--- EmailGenerator/Form1.cs
+++ EmailGenerator/Form1.cs
@@ -16,6 +16,7 @@ namespace EmailGenerator
         public Form1() {InitializeComponent();}
         private void label1_Click(object sender, EventArgs e){}//игнорируем
 
+        
         public static string genName()// генерирует название ящика
         {
             string name_func = "";
@@ -31,7 +32,61 @@ namespace EmailGenerator
             }
             return name_func;
         }
+        public string genSecondDomain()// генерирунт домен второго уровня
+        {
+            if (Checkbox_defaultdomain.Checked == true) 
+            { return Default_SecondLevelDomain.Text; }
+            string name_second_domain = "";
+            string sogl = "qwrtpsdfghjklzxcvbnm";
+            string glas = "eyuioa";
+            for (int i = 0; i < random.Next(3, 8); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    name_second_domain += sogl[random.Next(sogl.Length)];
+                }
+                else { name_second_domain += glas[random.Next(glas.Length)]; }
+            }
+            return name_second_domain;
+        }
+        public  string genFirstDomain()// генерирует домен первого уровня
+        {
+            if (Checkbox_defaultdomain.Checked == true)
+            { return default_FirstLeveldomain.Text; }
+            string name_first_domain = "";
+            string alph = "qwrtpsdfghjklzxcvbnmeyuioa";
+            for (int i = 0; i < random.Next(2, 4); i++)
+            {
+                name_first_domain += alph[random.Next(alph.Length)];
+            }
+            return name_first_domain;
+        }
 
+        public  string genFullEmail()// генерирует полный адрес
+        {
+            string full_email = $"{genName()}@{genSecondDomain()}.{genFirstDomain()}\n";
+            return full_email;
+        }
 
+        public string gen_mail_password() //Генерирует пароль для почты
+        {
+            string password_mail = "";
+            string password_symbols = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_@%*";
+            for (int i = 0; i < Convert.ToInt32(Password_Length.Text); i++)
+            {
+                password_mail += password_symbols[random.Next(password_symbols.Length)];
+            }
+            return password_mail;
+        }
+
+        private void btn_GenerateEmails_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Convert.ToInt32(EmailsNumText.Text); i++) 
+            { 
+                MainConsole.Text += $"{genFullEmail()} {gen_mail_password()}\n";
+                MainConsole.Text += "----------------------------------------------- \n";
+            }
+            
+        }
     }
 }
