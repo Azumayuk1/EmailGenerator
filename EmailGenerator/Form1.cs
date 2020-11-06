ab7109f011ec36f80680a8a7d64fc1be85ef1269
CaptainProninInSpace <sergey_rozhkov1@mail.ru>
2020-11-06 13:42:45 +0300

    Update Form1.cs

--- EmailGenerator/Form1.cs
+++ EmailGenerator/Form1.cs
@@ -12,13 +12,26 @@ namespace EmailGenerator
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        public static Random random = new Random();// RANDOM
+        public Form1() {InitializeComponent();}
         private void label1_Click(object sender, EventArgs e){}//игнорируем
 
-        
-        
+        public static string genName()// генерирует название ящика
+        {
+            string name_func = "";
+            string sogl = "qwrtpsdfghjklzxcvbnm";
+            string glas = "eyuioa";
+            for (int i = 0; i < random.Next(4, 12); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    name_func += sogl[random.Next(sogl.Length)];
+                }
+                else { name_func += glas[random.Next(glas.Length)]; }
+            }
+            return name_func;
+        }
+
+
     }
 }
